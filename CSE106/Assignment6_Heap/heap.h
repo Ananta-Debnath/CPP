66c1eedb0b156d44b6db0131de3e5f317031916c
Ananta Debnath <ananta.deb2005@gmail.com>
2025-07-05 12:25:22 +0600

    (CSE106-Assignment6) Implement heap (without heapsort)

--- CSE106/Assignment6_Heap/heap.h
+++ CSE106/Assignment6_Heap/heap.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 
 class Heap
@@ -9,20 +10,112 @@ private:
     int length;
     int capacity;
 
-    void heapify();
+    void resize(int new_capacity)
+    {
+        if (new_capacity != capacity)
+        {
+            capacity = new_capacity;
+            int* newArray = new int[capacity];
+            for (int i = 0; i <= length; i++) newArray[i] = array[i];
+
+            delete[] array;
+            array = newArray;
+        }
+    }
+
+    int parent(int idx)
+    {
+        return idx / 2;
+    }
+
+    int left(int idx)
+    {
+        return 2 * idx;
+    }
+
+    int right(int idx)
+    {
+        return (2 * idx) + 1;
+    }
+
+    void maxHeapify(int idx)
+    {
+        int l = left(idx);
+        int r = right(idx);
+        int largest;
+
+        if (l <= length && array[l] > array[idx]) largest = l;
+
+        else largest = idx;
+
+        if (r <= length && array[r] > array[largest]) largest = r;
+
+        if (largest != idx)
+        {
+            int temp = array[idx];
+            array[idx] = array[largest];
+            array[largest] = temp;
+            maxHeapify(largest);
+        }
+    }
 
 public:
-    Heap(int size = 2);
+    Heap(int size = 2)
+    {
+        length = 0;
+        capacity = size + 1;
+        array = new int[capacity];
+    }
 
-    Heap(vector<int>& v);
+    Heap(vector<int>& v)
+    {
+        length = 0;
+        capacity = v.size() + 1;
+        array = new int[capacity];
 
-    void insert(int n);
+        for (int x : v) insert(x);
+    }
 
-    void deleteKey();
+    void insert(int key)
+    {
+        array[++length] = key;
+        
+        int idx = length;
+        int par = parent(idx);
+        while (par != 0 && array[par] < array[idx])
+        {
+            int temp = array[idx];
+            array[idx] = array[par];
+            array[par] = temp;
+            idx = par;
+            par = parent(idx);
+        }
+        if (length + 1 >= capacity) resize(capacity*2);
+        
+        // cout << "Insert " << key << endl;
+        // for (int i = 0; i <= length; i++) cout << array[i] << " ";
+        // cout << endl;
+    }
 
-    int size() const;
+    void deleteKey()
+    {
+        if (length > 0)
+        {
+            array[1] = array[length--];
+            maxHeapify(1);
+            if (length * 4 < capacity && capacity > 10) resize(capacity / 2);
+        }
+    }
 
-    int getMax() const;
+    int size() const
+    {
+        return length;
+    }
+
+    int getMax() const
+    {
+        return array[1];
+    }
 };
 
 // void heapsort(vector<int>& v);
