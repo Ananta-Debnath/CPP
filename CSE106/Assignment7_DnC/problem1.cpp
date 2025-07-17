49a1bd04f1d39c591cf3afd60771389230567043
Ananta Debnath <ananta.deb2005@gmail.com>
2025-07-17 22:12:42 +0600

    (CSE106-Assignment7) Solve problem 1

---
+++ CSE106/Assignment7_DnC/problem1.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool isPossible(const vector<int>& workloads, int workload, int employeeCount)
+{
+    int sum = 0;
+
+    for (int x : workloads)
+    {
+        sum += x;
+        if (sum > workload)
+        {
+            sum = x;
+            employeeCount--;
+        }
+    }
+    return (employeeCount > 0);
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> workloads;
+    int start = 0, end = 0;
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        workloads.push_back(temp);
+
+        end += temp; // total
+        if (temp > start) start = temp; // min possible workload
+    }
+
+    int m, res;
+    if (workloads.size() < k) m = -1;
+
+    else
+    {
+        while (start <= end)
+        {
+            m = (start + end)/2;
+
+            if(isPossible(workloads, m, k))
+            {
+                res = m;
+                end = m - 1;
+            }
+
+            else start = m + 1;
+        }
+    }
+    cout << res << endl;
+
+    return 0;
+}
--- CSE106/Assignment7_DnC/problem2.cpp
+++ CSE106/Assignment7_DnC/problem2.cpp
@@ -50,8 +50,6 @@ int main()
 
     delete[] seats;
 
-    main();
-
     return 0;
 }
 
